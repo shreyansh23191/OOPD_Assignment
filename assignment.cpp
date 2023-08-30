c1e9a59397412e838fca05f5b1957b2558db4240
codimi <shreyansh20012@gmail.com>
2023-08-30 22:04:37 +0530

    Taking Input from user for year and amount invest

--- assignment.cpp
+++ assignment.cpp
@@ -10,7 +10,20 @@ int main() {
         cerr << "Failed to open the Myfile." << std::endl;
         return 1;
     }
-    
+
+    double amount, amount1, amount2,amount3;
+    int year;
+
+    cout << "Enter amount invested: " << std::endl;
+    cin >> amount2;
+    cout << "Enter the year in which amount is invested: " << std::endl;
+    cin >> year;
+    amount = amount2;
+    string line;
+    int serialnum, fileYear;
+    long double interestRate, inflationRate;
+        
+
     Myfile.close();
 
     return 0;
