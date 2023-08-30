e4987fad5ae5c1661aea877fcc3ed715eb186397
codimi <shreyansh20012@gmail.com>
2023-08-30 22:10:07 +0530

    Main logic for code

--- assignment.cpp
+++ assignment.cpp
@@ -22,8 +22,18 @@ int main() {
     string line;
     int serialnum, fileYear;
     long double interestRate, inflationRate;
-        
+    while (getline(Myfile, line)) {
+        istringstream iss(line);
+        char comma;
 
+        if (iss >> serialnum >> comma >> fileYear >> comma >> interestRate >> comma >> inflationRate) {
+            if (fileYear >= year) {
+                amount1 = amount * (1 + (interestRate) / 100);
+                amount = amount1;
+                
+
+            }
+        }
     Myfile.close();
 
     return 0;
