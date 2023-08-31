531641ca3371ee748273371416473a3711906bf6
codimi <shreyansh20012@gmail.com>
2023-08-31 14:23:19 +0530

    Readme file

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # OOPD_Assignment
+
+
+This program calculates the actual amount of money received and the value of money received based on the investment information stored in the CSV file. It reads the data in the 'india_data.csv' file, performs calculations and provides insight into investment results.
+
+## Function
+
+- Reads the investment data from CSV file
+- Calculates the amount obtained after adding interest
+- Determines the amount of money received considering inflation rates
+- Show results upto 2 decimal places
+
+
+##
+
+1. The program will ask you to enter the value of your investment and the year investment was made.
+3. The program will read the file "india_data.csv" and calculate the actual amount received and the amount received subtracted by inflation based on the given data.
+4. For rounding off upto 2 decimals ,set precision is used which sets all the outputs to 2 decimal places
+
+## Comments
+
+- This program is designed to read "india_data.csv" from the same directory as the executable.
+- Calculate values ​​equal to two decimal places for clarity.
+
+## References
+
+1. My references include Chatgpt to learn how to read csv file and perform operations on it
--- assignment.cpp
+++ assignment.cpp
@@ -4,49 +4,59 @@
 #include <iomanip>
 
 using namespace std;
-
 int main() {
     ifstream Myfile("india_data.csv");
     if (!Myfile.is_open()) {
-        cerr << "Failed to open the Myfile." << std::endl;
+        cerr << "Failed to open the Myfile." << endl;
         return 1;
     }
 
-    double amount, amount1, amount2,amount3;
+    double amount, amount1, amount2,amount3,amount4;
     int year;
 
-    cout << "Enter amount invested: " << std::endl;
+    cout << "Enter amount invested: " << endl;
     cin >> amount2;
-    cout << "Enter the year in which amount is invested: " << std::endl;
+    cout << "Enter the year in which amount is invested: " << endl;
     cin >> year;
-    amount = amount2;
+    if(year<2002){
+        amount2 = amount2/166;
+    }
+    amount1 = amount2;
+    amount3 = amount2;
+    amount4 = amount2;
     string line;
     int serialnum, fileYear;
     long double interestRate, inflationRate;
     while (getline(Myfile, line)) {
         istringstream iss(line);
         char comma;
-
+        
         if (iss >> serialnum >> comma >> fileYear >> comma >> interestRate >> comma >> inflationRate) {
+            if (fileYear < 1978){ 
+                amount3 = (amount3 * (1 + (interestRate - inflationRate) / 100))/18;
+            }
+            if (fileYear >= 1978){
+                amount4 = amount4 * ((1 + (interestRate - inflationRate) / 100));
+            }
+            
             if (fileYear >= year) {
-                amount1 = amount * (1 + (interestRate) / 100);
-                amount = amount1;
+                amount1 = amount1 * (1 + (interestRate) / 100); 
                 
 
-                amount3 = amount2 * (1 + (interestRate - inflationRate) / 100);
-                amount2 = amount3;
-
+                amount2 = amount2 * (1 + (interestRate - inflationRate) / 100);
+                
                 }
                 
         } else {
             cerr << "Failed to parse line: " << line << endl;
         }
-        cout<<fixed<<setprecision(2);
-        cout << "Year: " << fileYear << " - Money actually received: " << amount1 << " - Value of money actually received: " << amount2 << std::endl;
-    
 
     }
-            
+    cout<<fixed<<setprecision(2);
+    cout << "Year: " << fileYear << " - Money received: " << amount1 << " - Value of money received: " << amount2 << std::endl;
+    if(amount3<amount4){
+        cout<<"Dictatorship wins";
+            }   
 
     Myfile.close();
 
